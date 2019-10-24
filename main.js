a84b6a0215910c9f9e38c114b87de437923398e2
vishalsingh28 <55126408+vishalsingh28@users.noreply.github.com>
2019-10-24 19:40:16 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -7,9 +7,9 @@ $(document).ready(function() {
     });
 });
 $(".rotate").textrotator({
-    animation: "flipUp", // You can pick the way it animates when rotating through words. Options are dissolve (default), fade, flip, flipUp, flipCube, flipCubeUp and spin.
-    separator: ",", // If you don't want commas to be the separator, you can define a new separator (|, &, * etc.) by yourself using this field.
-    speed: 2000 // How many milliseconds until the next word show.
+    animation: "flipUp",
+    separator: ",",
+    speed: 2000
 });
 AOS.init({
     duration: 2000,
